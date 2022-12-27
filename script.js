bc9117e21b137c59c8ac8c1ee8b5477761d7d9f1
Abhinav Salgania <abhinavsalgania@gmail.com>
2022-12-27 15:54:08 -0330

    Add Pexels API to display image of the city searched.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Weather API key: f44d49ad0e3ac24ab2893e563a92fb01
 // API call: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//Pexels API key: 563492ad6f917000010000012c96493ede4642d586f35fcdd10a21ff
+// LocatinIQ API key: pk.f991767cfec1ae3b2800aaf250e29061
+
+
 // Weather APP
 
 //Function to get the weather data from the API
@@ -9,6 +13,24 @@ async function getWeatherData(city) {
     const weatherData = await response.json();
     console.log(weatherData);
     return weatherData;
+}
+// Function to display the weather data for the default city
+
+// Pexels API
+
+async function getImage(city) {
+    const response = await fetch(`https://api.pexels.com/v1/search?query=${city}&per_page=1`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: '563492ad6f917000010000012c96493ede4642d586f35fcdd10a21ff' , 
+        },
+    });
+    const imageData = await response.json();
+    const src= imageData.photos[0].src.original;
+    console.log(imageData);
+
+    return src;
 }
 
 //Get the weather data and process it
@@ -46,15 +68,21 @@ function displayWeather(weather) {
 
     const icon = document.querySelector('.icon');
     icon.src = `http://openweathermap.org/img/wn/${weather.icon}.png`;
-
 }
 
-// Function to display the weather data
+function displayImage(image) {
+    const cityimage = document.querySelector('.cityimg');
+    cityimage.src = image;
+}
+
+// Function to display the city image
 
 async function show(city) {
     const data = await getWeatherData(city);
     const weather = processData(data);
     displayWeather(weather);
+    const image = await getImage(city);
+    displayImage(image);
 }
 
 // Function to get the city name from the input field
@@ -71,6 +99,8 @@ searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const city = getCity();
     show(city);
+    getImage(city); 
+
     clearInput();
 });
 
@@ -81,5 +111,6 @@ function clearInput() {
 }
 
 clearInput(); //clear the input field on page load
+
 
 

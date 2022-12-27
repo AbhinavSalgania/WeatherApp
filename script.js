7f0095ede5e7be91d9be5a12d921b4a9f000e5a3
Abhinav Salgania <abhinavsalgania@gmail.com>
2022-12-27 16:47:34 -0330

    add function to display weather and image for user's location.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Weather API key: f44d49ad0e3ac24ab2893e563a92fb01
 // API call: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 //Pexels API key: 563492ad6f917000010000012c96493ede4642d586f35fcdd10a21ff
-// LocatinIQ API key: pk.f991767cfec1ae3b2800aaf250e29061
+
 
 
 // Weather APP
@@ -112,5 +112,53 @@ function clearInput() {
 
 clearInput(); //clear the input field on page load
 
+//get coordinates from the geolocation API of user's location
 
+async function getCoordinates() {
+    try {
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      return { latitude, longitude };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+  
+// After getting coordinates,
+// use OpenWeather API to get weather data for the user's location
 
+async function getWeatherDataByCoordinates() {
+    try {
+        const coordinates = await getCoordinates();
+        const { latitude, longitude } = coordinates;
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f44d49ad0e3ac24ab2893e563a92fb01&units=metric`);
+        const weatherData = await response.json();
+        console.log(weatherData);
+        return weatherData;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+// After getting weather data, display it
+
+async function displayWeatherDataByCoordinates() {
+    try {
+        const weatherData = await getWeatherDataByCoordinates();
+        const weather = processData(weatherData);
+        displayWeather(weather);
+        const city = weather.city;
+        getImage(city);
+        const image = await getImage(city);
+        displayImage(image);
+    } catch (error) {
+        console.log(error);
+    }
+    }
+    
+displayWeatherDataByCoordinates();
